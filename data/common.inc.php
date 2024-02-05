daa9a92dc8c567704e5f55e2753c44589a9fffbd
Xiaoqing GUO (Julie) <45688362+GuoJulie@users.noreply.github.com>
2024-02-06 03:54:45 +0800

    Add files via upload

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'hongrun';
+$cfg_dbuser = 'hongrun';
+$cfg_dbpwd = 'b5b7t4e8';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
